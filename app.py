81b95ade4798ba43f5b5a89691395b035e68e384
Paige Moore <114637224+Paijisan@users.noreply.github.com>
2025-12-28 15:02:39 -0800

    successfully saves games in database

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
-from flask import Flask, request , render_template
+from flask import Flask, request, render_template, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
+
+def init_db():
+    conn = sqlite3.connect("game.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS games (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            platform TEXT NOT NULL,
+            status TEXT NOT NULL
+        )
+    """)
+
+    conn.commit()
+    conn.close()
+
+def add_game_to_db(title, platform, status):
+    conn = sqlite3.connect("game.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO games (title, platform, status) VALUES (?, ?, ?)",
+        (title, platform, status)
+    )
+
+    conn.commit()
+    conn.close()
+
+    print("Saved:", title, platform, status)
+
 
 
 
@@ -8,14 +40,18 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/add", methods=["POST"])
+@app.route("/add", methods=["GET", "POST"])
 def add_game():
-    title = request.form.get["title"]
-    platform = request.form.get["platform"]
-    status = request.form.get["status"]
+    title = request.form.get("title")
+    platform = request.form.get("platform")
+    status = request.form.get("status")
+    
+    add_game_to_db(title, platform, status)
+    return redirect(url_for("home"))
 
-    return f"Received: {title}, {platform}, {status}"
-
+    
 if __name__ == "__main__":
+    init_db()            
     app.run(debug=True)
+
     
