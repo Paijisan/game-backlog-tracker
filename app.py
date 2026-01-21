ac2ee9364936c4c37a0e0575ae1cd8e907dd6f65
Paige Moore <114637224+Paijisan@users.noreply.github.com>
2026-01-21 14:59:34 -0800

    added delete function

--- app.py
+++ app.py
@@ -34,11 +34,37 @@ def add_game_to_db(title, platform, status):
     print("Saved:", title, platform, status)
 
 
+def delete_game_from_db(game_id):
+    conn = sqlite3.connect("game.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "DELETE FROM games WHERE id = ?",
+        (game_id,)
+
+    )
+
+    conn.commit()
+    conn.close()
+    
+
+def get_games():
+    conn = sqlite3.connect("game.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id, title, platform, status FROM games ORDER BY id DESC")
+    games = cursor.fetchall()
+
+    conn.close()
+    return games
+
+
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    games = get_games()
+    return render_template("index.html", games=games)
 
 @app.route("/add", methods=["GET", "POST"])
 def add_game():
@@ -47,6 +73,12 @@ def add_game():
     status = request.form.get("status")
     
     add_game_to_db(title, platform, status)
+    return redirect(url_for("home"))
+
+
+@app.route("/delete/<int:game_id>", methods=["POST"])
+def delete_game(game_id):
+    delete_game_from_db(game_id)
     return redirect(url_for("home"))
 
     
